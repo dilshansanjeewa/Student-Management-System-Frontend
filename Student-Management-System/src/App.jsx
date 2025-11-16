68f73f599c66e70b7fab975281ef0cf4b3e88f8b
Dilshan Sanjeewa <dilshansanjeewads7@gmail.com>
2025-11-16 11:53:02 +0530

    Add Bootstrap setup and Header component

--- Student-Management-System/src/App.jsx
+++ Student-Management-System/src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1>Hello React</h1>
+    <Header/>
     </>
   )
 }
